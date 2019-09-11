bd0d6bce2cf4f3ff986e789805b528c07bcb1e7e
介川貴晶 <takaaki@sukegawa.me>
2019-09-11 20:36:28 +0900

    banner sp

--- src/components/ItemLists.js
+++ src/components/ItemLists.js
@@ -1,15 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Image from '../components/Image'
 
 export default function ItemLists(props) {
   const { data, columns , maxLength } = props;
-  const [itemListData, setItemListData] = useState([])
-  
-  useEffect(() => {
-    setItemListData(data);
-  },[]);
   return (
-    itemListData.map((List, Index) => {
+    data.map((List, Index) => {
       const filename = List.noimage ? 'no-image' : `${List.image}`;
       const MaxLength = maxLength !== undefined ? Index < maxLength : true;
       const textLength = List.text.length > 42 ? List.text.substr(0, 40) + '...' : List.text;
